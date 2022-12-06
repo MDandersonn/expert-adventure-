0598114e4acfa99862e6a5ec73d73d648c1d9efc
MDandersonn <kimyj1215@naver.com>
2022-12-06 21:46:16 +0900

    12.6 시험

--- oracle_sql02/Scripts/EXAM3.sql
+++ oracle_sql02/Scripts/EXAM3.sql
@@ -23,7 +23,28 @@ BEGIN
 	END IF;
 END;
 
+
+CREATE OR REPLACE FUNCTION GETDNAME2(vdepartment_id IN departments.department_id%TYPE)
+RETURN departments.department_name%TYPE
+IS
+  vdepartment_name departments.department_name%TYPE;
+ 
+BEGIN
+    SELECT department_name INTO vdepartment_name 
+    FROM departments
+    WHERE department_id = vdepartment_id; 
+   	RETURN vdepartment_name;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('해당부서없다..');
+		return '해당 부 서없 음';
+END;
+SELECT * FROM USER_ERRORS;
+
+
 SELECT GETDNAME(1000) FROM DUAL;
+
+SELECT GETDNAME2(1000) FROM DUAL;
 
 
 
